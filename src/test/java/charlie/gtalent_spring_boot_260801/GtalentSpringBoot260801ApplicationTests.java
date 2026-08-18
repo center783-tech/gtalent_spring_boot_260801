1f867d39081de26912ab473de6edf4a44a509bc5
center783-tech <center783@gmail.com>
2026-08-18 21:27:20 +0800

    Step 1: Upgrade Java runtime to 25 and fix test-time mail bean startup

--- src/test/java/charlie/gtalent_spring_boot_260801/GtalentSpringBoot260801ApplicationTests.java
+++ src/test/java/charlie/gtalent_spring_boot_260801/GtalentSpringBoot260801ApplicationTests.java
@@ -2,8 +2,10 @@ package charlie.gtalent_spring_boot_260801;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestMailConfig.class)
 class GtalentSpringBoot260801ApplicationTests {
 
 	@Test
