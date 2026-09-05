aff4f613dd2c1aed1fc9607489be20bdec6ed4c2
center783-tech <center783@gmail.com>
2026-09-05 14:02:37 +0800

    版本控制16 0905

--- src/main/java/charlie/gtalent_spring_boot_260801/controller/PageController.java
+++ src/main/java/charlie/gtalent_spring_boot_260801/controller/PageController.java
@@ -22,6 +22,11 @@ public class PageController {
         return "login";
     }
 
+    @GetMapping("/page/register")
+    public String registerPage() {
+        return "register";
+    }
+
     @GetMapping("/page/books")
     public String bookPage(){
         return "books";
